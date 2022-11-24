b2a546d41e40aa2aa7510e3e3e383a2396168816
Suphadet Vatjanajaroenrat <suphadet.b@gmail.com>
2022-11-24 18:40:52 +0700

    draft 1 certificates

--- src/components/Certificate/Certificate.js
+++ src/components/Certificate/Certificate.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
-import { } from './CertificateStyles';
+import { GalleryContainer, Thumbnails, Slides, ImageBox, Image } from './CertificateStyles';
 
 function Certificate() {
   
@@ -20,6 +20,16 @@ function Certificate() {
     <Section id="certificates">
         <SectionDivider divider />
         <SectionTitle dark={darkTrue}>Certificates</SectionTitle>
+        <GalleryContainer>
+            <Thumbnails></Thumbnails>
+            <Slides>
+                <ImageBox><Image src="https://raw.githubusercontent.com/darking2539/boss-cv/main/public/images_pdf/k8s%20mastery.png"/></ImageBox>
+                <ImageBox><Image src="https://raw.githubusercontent.com/darking2539/boss-cv/main/public/images_pdf/DAB.jpg"/></ImageBox>
+                <ImageBox><Image src="https://raw.githubusercontent.com/darking2539/boss-cv/main/public/images_pdf/introdocker.png"/></ImageBox>
+                <ImageBox><Image src="https://raw.githubusercontent.com/darking2539/boss-cv/main/public/images_pdf/devsecops.png"/></ImageBox>
+                <ImageBox><Image src="https://raw.githubusercontent.com/darking2539/boss-cv/main/public/images_pdf/Reactjs.jpg"/></ImageBox>
+            </Slides>
+        </GalleryContainer>
     </Section>
   );
 };
--- src/components/Certificate/CertificateStyles.js
+++ src/components/Certificate/CertificateStyles.js
@@ -1,117 +1,61 @@
 import styled from 'styled-components';
 
-export const Img = styled.img`
-  border-radius: 10px 10px 0px 0px;
-  box-shadow: 3px 3px 20px rgba(80, 78, 78, 0.5);
-  width:100%;
-  height:100%;
-  object-fit: cover;
-  overflow: hidden;
-`
-
-export const GridContainer = styled.section`
-display: grid;
-grid-template-columns: repeat(auto-fill, minmax(400px, 1fr));
-padding: 3rem;
-place-items: center;
-column-gap: 2rem;
-row-gap: 3rem;
-@media ${(props) => props.theme.breakpoints.sm} {
+export const GalleryContainer = styled.div`
+  position: relative;
   display: flex;
-  flex-direction: column;
-  padding: 2rem;
-  padding-bottom: 0;
-}
+  justify-content: center;
+`;
 
+export const Thumbnails = styled.div`
+  position: absolute;
+  bottom: 8px;
+  display: flex;
+  flex-direction: row;
+  gap: 6px;
 `
-export const BlogCard = styled.div`
-  margin-bottom: 20px;
-  border-radius: 10px;
-  box-shadow: 3px 3px 20px rgba(80, 78, 78, 0.5);
-  text-align: center;
-  width: 400px;
-  color: ${(props) => props.dark ? '#0F1624' : '#F2FFFF'};
-  background: ${(props) => props.dark ? '#F2FFFF' : '#0F1624'}; 
-  @media ${(props) => props.theme.breakpoints.sm} {
-    width: 100%;
+export const Slides = styled.div`
+  margin: 0 16px;
+  display: grid;
+  grid-auto-flow: column;
+  gap: 1rem;
+  width: 800px;
+  padding: 0 0.25rem;
+  height: 620px;
+  overflow-y: auto;
+  overscroll-behavior-x: contain;
+  scroll-snap-type: x mandatory;
+  scrollbar-width: none;
+
+  @media ${props => props.theme.breakpoints.lg} {
+    width: 650px;
+    height: 550px;
+	}
+
+  @media ${props => props.theme.breakpoints.md}{
+    width: 580px;
+    height: 460px;
   }
-`;
-export const TitleContent = styled.div`
-  text-align: center;
-  z-index: 20;
-  width: 100%;
-
-`;
-
-
-export const HeaderThree = styled.h3`
-  font-weight: 500;
-  letter-spacing: 2px;
-  color: #9cc9e3;
-  padding: .5rem 0;
-  font-size: ${(props) => props.title ? '3rem' : '2rem'};
-`;
-
-export const Hr = styled.hr`
-  width: 50px;
-  height: 3px;
-  margin: 20px auto;
-  border: 0;
-  background: #d0bb57;
-`;
-
-export const Intro = styled.div`
-  width: 170px;
-  margin: 0 auto;
-  color: #dce3e7;
-  font-family: 'Droid Serif', serif;
-  font-size: 13px;
-  font-style: italic;
-  line-height: 18px;
-`;
-
-
-export const CardInfo = styled.p`
-  width: 100%;
-  padding: 0 30px;
-  color: ${(props) => props.dark ? '#0F1624' : '#F2FFFF'};
-  font-style: 2rem;
-  line-height: 24px;
-  text-align: justify;
-  @media ${(props) => props.theme.breakpoints.sm} {
-    padding: 2rem;
-  
-}
-`;
-
-
-export const UtilityList = styled.ul`
-  list-style-type: none;
-  padding: 0;
-  display: flex;
-  justify-content: space-around;
-  margin: 2.5rem 0;
-`;
-
-export const ExternalLinks = styled.a`
-color:#d4c0c0;
-font-size: 1.6rem;
-padding:1rem 1.5rem;
-background: #6b3030;
-border-radius: 15px;
-transition: 0.5s;
-&:hover{
-  background: #801414;
-
-}
-`;
-
-export const TagList = styled.ul`
-display: flex;
-justify-content: space-around;
-padding: 2rem;
+  @media ${props => props.theme.breakpoints.sm}{
+    width: 320px;
+    height: 250px;
+  }
 `
-export const Tag = styled.li`
-color: #d8bfbf;
-font-size: 1.5rem;
+export const ImageBox = styled.div`
+  scroll-snap-align: start;
+`
+
+export const Image = styled.img`
+  width: 800px;
+  object-fit: cover;
+
+  @media ${props => props.theme.breakpoints.lg} {
+    width: 650px;
+	}
+
+  @media ${props => props.theme.breakpoints.md}{
+    width: 580px;
+  }
+  @media ${props => props.theme.breakpoints.sm}{
+    width: 320px;
+  }
 `
